b2190f198aa7ad0fbbb52aa500f4d310b0907198
ChandradipMoonlight <chandradipshivankar@gmail.com>
2021-05-13 16:16:44 +0530

    Start position added

--- SnakeLadderGame.java
+++ SnakeLadderGame.java
@@ -2,8 +2,11 @@ package javapractice;
 
 public class SnakeLadderGame {
 	public static void main(String[] args) {
-		System.out.println("Wellcome to the Snake Ladder game.");
+		System.out.println("Wellcome to the Snake and Ladder game.");
+		
+		int startPostion=0;
+	
 	}
-	}
+}
 
 
